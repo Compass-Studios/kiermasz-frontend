44417e58f40a4a74c8b6cfed8b6037c53e4b3bcd
S4vyss <mlodywojownik345@gmail.com>
2022-09-11 17:32:09 +0200

    Started routing and land page

---
+++ src/lib/components/headers/Header.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+    import { Router, Route, Link } from "svelte-routing";
+    import LoginPage from "../routes/login/LoginPage.svelte";
+</script>
+
+<header>
+    <Router>
+        <nav>
+            <ul>
+                <li>
+                    <Link to="logowanie">
+                        logowanie
+                    </Link>
+                </li>
+            </ul>
+        </nav>
+        <Route path="logowanie">
+            <LoginPage />
+        </Route>
+    </Router>
+</header>
+
+<style type="text/scss">
+
+</style>
